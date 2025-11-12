e4599291b16c54fce39aa3bbc80a16f3b4c721e3
eexy <eexdhe@gmail.com>
2025-11-12 17:03:47 +0100

    chore: create command to get game info by steam appid

--- src-tauri/src/commands/steam.rs
+++ src-tauri/src/commands/steam.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 use tauri_plugin_http::reqwest;
 
@@ -30,15 +32,60 @@ pub async fn get_steam_owned_games(
         steam_key, profile_id
     );
 
-    println!("{}", url);
+    let res = reqwest::get(&url).await.map_err(|e| e.to_string())?;
+
+    let body = res.text().await.map_err(|e| e.to_string())?;
+
+    let api_response: SteamApiResponse = serde_json::from_str(&body).map_err(|e| e.to_string())?;
+
+    Ok(api_response.response)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SteamGameGenre {
+    id: String,
+    description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SteamGameInfo {
+    name: String,
+    steam_appid: u32,
+    required_age: u32,
+    is_free: bool,
+    about_the_game: String,
+    header_image: String,
+    capsule_image: String,
+    genres: Vec<SteamGameGenre>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GameInfoData {
+    success: bool,
+    data: SteamGameInfo,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SteamGameInfoResponse {
+    #[serde(flatten)]
+    pub games: HashMap<String, GameInfoData>,
+}
+
+#[tauri::command]
+pub async fn get_game_info(game_id: u32) -> Result<HashMap<String, GameInfoData>, String> {
+    let url = format!(
+        "https://store.steampowered.com/api/appdetails?appids={}",
+        game_id
+    );
 
     let res = reqwest::get(&url).await.map_err(|e| e.to_string())?;
 
     let body = res.text().await.map_err(|e| e.to_string())?;
 
-    println!("{}", body);
+    println!("{}", &body);
 
-    let api_response: SteamApiResponse = serde_json::from_str(&body).map_err(|e| e.to_string())?;
+    let api_response: SteamGameInfoResponse =
+        serde_json::from_str(&body).map_err(|e| e.to_string())?;
 
-    Ok(api_response.response)
+    Ok(api_response.games)
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,10 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![commands::get_steam_owned_games])
+        .invoke_handler(tauri::generate_handler![
+            commands::get_steam_owned_games,
+            commands::get_game_info
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
