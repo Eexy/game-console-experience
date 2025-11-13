03ad265dedc0f355ca35ffdeb3184bbd8fb19d9e
eexy <eexdhe@gmail.com>
2025-11-13 19:43:43 +0100

    chore: load game via profilId in env to have better dev experience

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,36 @@
 </template>
 
 <script setup lang="ts">
+import { invoke } from "@tauri-apps/api/core";
+import { onMounted } from "vue";
 import { RouterView } from "vue-router";
+import { SteamOwnedGameResponse } from "./types/steam/api";
+import { storeToRefs } from "pinia";
+import { useGameStore } from "./stores/game.store";
+
+const { games } = storeToRefs(useGameStore());
+
+onMounted(async () => {
+    const key = import.meta.env.VITE_STEAM_KEY;
+    const profileId = import.meta.env.VITE_STEAM_PROFILE_ID;
+
+    if (!key) throw new Error("missing steam api key");
+    if (!profileId) throw new Error("missing steam profileId");
+
+    try {
+        const res: SteamOwnedGameResponse = await invoke(
+            "get_steam_owned_games",
+            {
+                profileId: profileId,
+                steamKey: key,
+            },
+        );
+
+        games.value = res.games;
+    } catch (e) {
+        console.error(e);
+    }
+});
 </script>
 
 <style scoped></style>
