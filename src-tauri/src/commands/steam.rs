48afa32c9b0adcc6b83b8bad97c72839dc576072
eexy <eexdhe@gmail.com>
2025-11-15 21:57:22 +0100

    feat: detect steam path instead of hardcoding it in source by checking windows registry

--- src-tauri/src/commands/steam.rs
+++ src-tauri/src/commands/steam.rs
@@ -1,8 +1,11 @@
+#[cfg(target_os = "windows")]
+use std::path::PathBuf;
 use std::{collections::HashMap, path::Path};
 
 use serde::{Deserialize, Serialize};
 use tauri_plugin_http::reqwest;
 use tauri_plugin_opener::OpenerExt;
+use windows_registry::LOCAL_MACHINE;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SteamOwnedGame {
@@ -95,8 +98,6 @@ pub async fn get_game_info(game_id: u32) -> Result<HashMap<String, GameInfoData>
 
     let body = res.text().await.map_err(|e| e.to_string())?;
 
-    println!("{}", &body);
-
     let api_response: SteamGameInfoResponse =
         serde_json::from_str(&body).map_err(|e| e.to_string())?;
 
@@ -105,11 +106,13 @@ pub async fn get_game_info(game_id: u32) -> Result<HashMap<String, GameInfoData>
 
 #[tauri::command]
 pub async fn is_game_installed(game_id: u32) -> bool {
-    let file_path = format!(
-        "C:\\Program Files (x86)\\Steam\\steamapps\\appmanifest_{}.acf",
-        game_id,
-    );
-    let path = Path::new(&file_path);
+    let mut raw_steam_path = get_steam_path().expect("unable to get steam path");
+    raw_steam_path.push("steamapps");
+    raw_steam_path.push(format!("appmanifest_{}.acf", game_id));
+
+    let steam_path = raw_steam_path.to_str().expect("unable to get steam path");
+
+    let path = Path::new(&steam_path);
 
     path.exists()
 }
@@ -151,4 +154,37 @@ pub fn filter_games(games: Vec<SteamOwnedGame>, search: String) -> Vec<SteamOwne
 fn sort_game(mut games: Vec<SteamOwnedGame>) -> Vec<SteamOwnedGame> {
     games.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
     games
+}
+
+fn get_steam_path() -> Option<PathBuf> {
+    #[cfg(windows)]
+    {
+        get_steam_path_windows()
+    }
+
+    #[cfg(not(windows))]
+    {
+        None
+    }
+}
+
+#[cfg(target_os = "windows")]
+fn get_steam_path_windows() -> Option<PathBuf> {
+    //  LOCAL_MACHINE
+    if let Ok(install_path) = LOCAL_MACHINE
+        .open(r"SOFTWARE\WOW6432Node\Valve\Steam")
+        .and_then(|key| key.get_string("InstallPath"))
+    {
+        return Some(PathBuf::from(install_path));
+    }
+
+    //  CURRENT_USER
+    if let Ok(install_path) = LOCAL_MACHINE
+        .open(r"Software\Valve\Steam")
+        .and_then(|key| key.get_string("SteamPath"))
+    {
+        return Some(PathBuf::from(install_path));
+    }
+
+    None
 }
