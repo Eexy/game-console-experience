df8bf0e375ae09f10b176d1183f48ccbcd011477
eexy <eexdhe@gmail.com>
2025-11-17 19:33:49 +0100

    feat: only refresh game list on refresh by using a flag in session storage

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
 <script setup lang="ts">
 import { invoke } from "@tauri-apps/api/core";
-import { onMounted } from "vue";
 import { RouterView } from "vue-router";
 import { storeToRefs } from "pinia";
 import { useGameStore } from "./stores/game.store";
@@ -12,36 +11,34 @@ import { GameItem } from "./types/games";
 
 const { games } = storeToRefs(useGameStore());
 
-onMounted(async () => {
-    const key = import.meta.env.VITE_STEAM_KEY;
-    const profileId = import.meta.env.VITE_STEAM_PROFILE_ID;
-
-    if (!key) throw new Error("missing steam api key");
-    if (!profileId) throw new Error("missing steam profileId");
-
-    try {
-        const res: GameItem[] = await invoke("get_games", {
-            profileId: profileId,
-            steamKey: key,
-        });
-        games.value = res;
-    } catch (e) {
-        console.log(e);
-    }
-});
-
 window.addEventListener("load", async () => {
     const key = import.meta.env.VITE_STEAM_KEY;
     const profileId = import.meta.env.VITE_STEAM_PROFILE_ID;
 
-    try {
-        await invoke("refresh_games", {
-            profileId: profileId,
-            steamKey: key,
-        });
-    } catch (e) {
-        console.error(e);
+    const isRefresh = sessionStorage.getItem("appLoaded");
+
+    if (!isRefresh || !Boolean(isRefresh)) {
+        try {
+            const res: GameItem[] = await invoke("get_games", {
+                profileId: profileId,
+                steamKey: key,
+            });
+            games.value = res;
+        } catch (e) {
+            console.log(e);
+        }
+    } else {
+        try {
+            await invoke("refresh_games", {
+                profileId: profileId,
+                steamKey: key,
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
+
+    sessionStorage.setItem("appLoaded", "true");
 });
 </script>
 
