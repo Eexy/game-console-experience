d43010c020a6a2c6ac6089facb3ff2bcec4bbf0f
eexy <eexdhe@gmail.com>
2025-11-18 19:25:22 +0100

    feat: update component to show game info

--- src/views/games/views/game/GameView.vue
+++ src/views/games/views/game/GameView.vue
@@ -1,7 +1,32 @@
 <template>
-    <div class="flex flex-col gap-2 p-2"></div>
+    <div class="flex flex-col gap-2 p-2">
+        <template v-if="game">
+            <h1 class="font-bold text-xl">{{ game.title }}</h1>
+        </template>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { GameItem } from "@/types/games";
+import { invoke } from "@tauri-apps/api/core";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const game = ref<GameItem | null>(null);
+
+onMounted(async () => {
+    try {
+        const res: GameItem = await invoke("get_game_by_id", {
+            id: Number(route.params.id),
+        });
+
+        game.value = res;
+    } catch (error) {
+        console.error(error);
+    }
+});
+</script>
 
 <style scoped></style>
