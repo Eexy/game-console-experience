c3b47e032dbddb0137261603937b3a73d38355a7
eexy <eexdhe@gmail.com>
2025-11-19 21:37:01 +0100

    feat : display game description

--- src/views/games/views/game/GameView.vue
+++ src/views/games/views/game/GameView.vue
@@ -2,6 +2,7 @@
     <div class="flex flex-col gap-2 p-2">
         <template v-if="game">
             <h1 class="font-bold text-xl">{{ game.title }}</h1>
+            <p>{{ game.description }}</p>
         </template>
     </div>
 </template>
@@ -9,14 +10,14 @@
 <script setup lang="ts">
 import { GameItem } from "@/types/games";
 import { invoke } from "@tauri-apps/api/core";
-import { onMounted, ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 
 const game = ref<GameItem | null>(null);
 
-onMounted(async () => {
+watchEffect(async () => {
     try {
         const res: GameItem = await invoke("get_game_by_id", {
             id: Number(route.params.id),
